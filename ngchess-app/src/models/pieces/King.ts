6f42d58e1e4c09b8a29d624d929ff5cd9fe407be
Math <math.leroy@eocenegroup.com>
2024-07-22 10:15:57 +0200

    add king moves without chess check, no pun intended

--- ngchess-app/src/models/pieces/King.ts
+++ ngchess-app/src/models/pieces/King.ts
@@ -8,7 +8,40 @@ export class King extends Piece {
     super('King', 'K', '&#9812', '&#9818', color);
   }
   override getMoves(board: Board, currentX: number, currentY: number): Move[] {
-    throw new Error("Method not implemented.");
+    const result: Move[] = [];
+    const moves: Move[] = [];
+    const top =  currentX - 1;
+    const bot = currentX + 1;
+    const left =  currentY - 1;
+    const right = currentY + 1;
+
+    // simple all direction moves
+    moves.push(new Move(currentX, currentY, top, currentY, this.color, this));
+    moves.push(new Move(currentX, currentY, top, right, this.color, this));
+    moves.push(new Move(currentX, currentY, currentX, right, this.color, this));
+    moves.push(new Move(currentX, currentY, bot, right, this.color, this));
+    moves.push(new Move(currentX, currentY, bot, currentY, this.color, this));
+    moves.push(new Move(currentX, currentY, bot, left, this.color, this));
+    moves.push(new Move(currentX, currentY, currentX, left, this.color, this));
+    moves.push(new Move(currentX, currentY, top, left, this.color, this));
+
+    // ne garder que les moves (piece friendly)
+    moves.forEach((move) => {
+      if(board.isValidPosition(move.toX, move.toY) && 
+          this.isValidKingMove(move) && 
+          (board.isEmpty(move.toX, move.toY) || 
+          board.isOpponentPiece(move.toX, move.toY, move.color)
+        )){
+        result.push(move);
+      }
+    });
+
+    return result;
+  }
+  
+  private isValidKingMove(move: Move): boolean {
+    // TODO : virer les moves qui mettent en echec
+    return true;
   }
   override move(x: number, y: number): void {
     throw new Error("Method not implemented.");
